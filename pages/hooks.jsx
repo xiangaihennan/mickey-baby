661aea3a02a9121d78f3945f4a8b27fd700a2bf8
liuzeqi <liuzeqi@huohua.cn>
2021-11-09 11:14:54 +0800

    hooks

--- pages/hooks.jsx
+++ pages/hooks.jsx
@@ -2,41 +2,30 @@ import { useToggle } from '@/hooks/useToggle/index.js'
 import { useCallback, useEffect, useMemo, useRef } from 'react'
 export default function Hooks() {
   const [state, { toggle, setLeft, setRight }] = useToggle('123', '456')
-  console.log('ahooks')
-  const ref = useRef()
   const hooksTest = () => {
     toggle()
   }
   /**
-   * 缓存的是值 依赖不变就不会再次执行 即使在jsx渲染函数中调用也不会执行
-   * 类似vue computed  且useMemo
-   *
+   * 类似vue computed  。
+   * 缓存的是值 即使在jsx渲染模版中调用也不会执行。
+   * 下面的例子相当于useCallback 但组件内部state更新（非deps情况） 函数不会重新执行
    */
-  const ff = useMemo((params) => {
+  const memo = useMemo((params) => {
     console.log(`useMemo`)
-    return '666666666666666'
+    return () => '666666666666666'
   }, [])
   // 只缓存函数 如果在jsx渲染模版中调用 依然会执行
-
+  const callback = useCallback((params) => {
+    console.log(`useCallback`)
+  }, [])
   useEffect((params) => {
-    const bb = (params) => {
-      console.log(`useCallback`)
-    }
     console.log(`useEffect`)
-    console.log(ref, '999999999999999999999999')
-    bb()
   }, [])
   return (
     <div>
-      <button onClick={hooksTest}>222222222222222{state}</button>
-      <div ref={ref}>{ff}</div>
-      {/* <div
-        onClick={(params) => {
-          console.log(ref, ref.current.innerHTML, '99999999999999999999')
-        }}
-      >
-        hhhhhhhhhhh
-      </div> */}
+      <button onClick={hooksTest}>修改state状态{state}</button>
+      <div>{memo()}</div>
+      <div>{callback()}</div>
     </div>
   )
 }
