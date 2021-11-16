de043102e879d963bf866c1da10032a9698484b4
liuzeqi <liuzeqi@huohua.cn>
2021-11-16 14:18:53 +0800

    添加音乐

--- data/mediaMetaData.js
+++ data/mediaMetaData.js
@@ -1,8 +1,20 @@
 const mediaData = {
   musicList: [
     {
-      name: `给自己的歌`,
-      url: `http://1259397000.vod2.myqcloud.com/68dd9606vodcq1259397000/ada0b7bb8602268011025581393/f0.m4a`,
+      name: `undaground Rappa`,
+      url: `http://1259397000.vod2.myqcloud.com/68dd9606vodcq1259397000/20585f92387702291512409764/f0.mp3`,
+    },
+    {
+      name: `Party All Night`,
+      url: `http://1259397000.vod2.myqcloud.com/68dd9606vodcq1259397000/20c3bfd0387702291512467128/f0.m4a`,
+    },
+    {
+      name: `Anutha Night in L.A.`,
+      url: `http://1259397000.vod2.myqcloud.com/68dd9606vodcq1259397000/20695a68387702291512417611/f0.m4a`,
+    },
+    {
+      name: `Flavour of the Old School`,
+      url: `http://1259397000.vod2.myqcloud.com/68dd9606vodcq1259397000/0bc36a3c387702291508872657/f0.m4a`,
     },
     {
       name: `daladala`,
@@ -12,6 +24,10 @@ const mediaData = {
       name: `Santa Baby`,
       url: `http://1259397000.vod2.myqcloud.com/68dd9606vodcq1259397000/ad0d44658602268011178843663/f0.m4a`,
     },
+    // {
+    //   name: `给自己的歌`,
+    //   url: `http://1259397000.vod2.myqcloud.com/68dd9606vodcq1259397000/ada0b7bb8602268011025581393/f0.m4a`,
+    // },
   ],
 }
 export default mediaData
