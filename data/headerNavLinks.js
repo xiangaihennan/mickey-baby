cd4d47342efed8db9a72636ae7af326e8a5ad6bf
liuzeqi <liuzeqi@huohua.cn>
2021-11-22 11:20:38 +0800

    更新header

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,9 +1,9 @@
 const headerNavLinks = [
-  { href: '/blog', title: '文章' },
+  { href: '/blog', title: '博客' },
   { href: '/tags', title: '标签' },
-  { href: '/life', title: '照片墙' },
-  { href: '/hiphop', title: 'Hiphop' },
-  { href: '/about', title: `～` },
+  { href: '/life', title: '照片' },
+  { href: '/hiphop', title: 'HipHop' },
+  { href: '/about', title: `Me！` },
 ]
 
 export default headerNavLinks
