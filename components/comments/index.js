7818e239361b6fe3d1a35002f664fec251dedf9e
liuzeqi <liuzeqi@huohua.cn>
2021-11-30 19:24:53 +0800

    更新央视

--- components/comments/index.js
+++ components/comments/index.js
@@ -37,7 +37,12 @@ const Comments = ({ frontMatter }) => {
       break
   }
   return (
-    <div id="comment">
+    <div
+      id="comment"
+      style={{
+        border: 0,
+      }}
+    >
       {siteMetadata.comment && siteMetadata.comment.provider === 'giscus' && (
         <GiscusComponent mapping={term} />
       )}
