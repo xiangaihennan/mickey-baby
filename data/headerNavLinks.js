430f2c90518326b809bf9f3556d10647b82a83d4
liuzeqi <liuzeqi@huohua.cn>
2021-12-15 10:18:13 +0800

    关闭hiphop life面板

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,8 +1,8 @@
 const headerNavLinks = [
   { href: '/blog', title: '文章' },
   { href: '/tags', title: '标签' },
-  { href: '/life', title: '旅行' },
-  { href: '/hiphop', title: 'HipHop' },
+  // { href: '/life', title: '旅行' },
+  // { href: '/hiphop', title: 'HipHop' },
   { href: '/about', title: `Me！` },
 ]
 
